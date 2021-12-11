fc288995ae7330b615c5879c4170a484612728ed
Alan Miranda <dittler.a@gmail.com>
2021-12-11 16:22:50 -0300

    updates

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,11 +1,29 @@
+import { gql, useMutation } from "@apollo/client";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const ADD_SONG = gql`
+  mutation AddSong($title: String) {
+    addSong(title: $title) {
+      title
+    }
+  }
+`;
 
 export const SongCreate = () => {
   const [song, setSong] = useState("");
+  const [addSong, { data, loading, error }] = useMutation(ADD_SONG);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(song);
+    addSong({
+      variables: {
+        title: song,
+      },
+    });
+    setSong("");
+    navigate("/");
   };
   return (
     <div>
